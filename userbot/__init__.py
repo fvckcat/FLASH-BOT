1c49ec33dfbca7925876296beda77f438551e4e5
fvckcat <79221598+fvckcat@users.noreply.github.com>
2021-03-13 15:37:56 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -349,7 +349,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {} 🐝".format("🐝", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {} 💠".format("💠", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],

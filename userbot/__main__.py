4361eeb7ecad2f0afc6d715e8718173d4298ffd2
fvckcat <79221598+fvckcat@users.noreply.github.com>
2021-03-16 23:26:47 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"Bee-Userbot ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"FLASH-BOT ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
 
 
 if len(argv) not in (1, 3, 4):

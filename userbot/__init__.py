1b75342bbc4819459c5b5f2bfd04d9797651c240
fvckcat <79221598+fvckcat@users.noreply.github.com>
2021-03-19 10:21:06 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -365,10 +365,10 @@ def paginate_help(page_number, loaded_modules, prefix):
         ] + [
             (
                 custom.Button.inline(
-                    "❮❮❮❮❮", data="{}_prev({})".format(prefix, modulo_page)
+                    "❮❮❮", data="{}_prev({})".format(prefix, modulo_page)
                 ),
                 custom.Button.inline(
-                    "❯❯❯❯❯", data="{}_next({})".format(prefix, modulo_page)
+                    "❯❯❯", data="{}_next({})".format(prefix, modulo_page)
                 )
             )
         ]

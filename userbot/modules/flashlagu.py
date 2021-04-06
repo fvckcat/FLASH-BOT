fff5b15557a1015b3990bffb012e401b25e44b05
fvckcat <79221598+fvckcat@users.noreply.github.com>
2021-04-06 13:24:38 +0700

    Update and rename lagu.py to flashlagu.py

---
+++ userbot/modules/flashlagu.py
@@ -0,0 +1,164 @@
+# Thanks To Ultroid
+# Jangan Dihapuss!!!
+# Port By @Vckyouuu
+
+
+import json
+import os
+import random
+
+from lyrics_extractor import SongLyrics as sl
+from telethon.tl.types import DocumentAttributeAudio
+from youtube_dl import YoutubeDL
+from youtube_dl.utils import (
+    ContentTooShortError,
+    DownloadError,
+    ExtractorError,
+    GeoRestrictedError,
+    MaxDownloadsReached,
+    PostProcessingError,
+    UnavailableVideoError,
+    XAttrMetadataError,
+)
+from youtubesearchpython import SearchVideos
+
+from userbot.events import register
+from userbot import CMD_HELP
+
+
+@register(outgoing=True, pattern=r"^\.song (.*)")
+async def download_video(event):
+    a = event.text
+    if a[5] == "s":
+        return
+    await event.edit("Searching...")
+    url = event.pattern_match.group(1)
+    if not url:
+        return await event.edit("**Error**\nUsage - `.song <song name>`")
+    search = SearchVideos(url, offset=1, mode="json", max_results=1)
+    test = search.result()
+    p = json.loads(test)
+    q = p.get("search_result")
+    try:
+        url = q[0]["link"]
+    except BaseException:
+        return await event.edit("`No matching song found...`")
+    type = "audio"
+    await event.edit(f"`Preparing to download {url}...`")
+    if type == "audio":
+        opts = {
+            "format": "bestaudio",
+            "addmetadata": True,
+            "key": "FFmpegMetadata",
+            "writethumbnail": True,
+            "prefer_ffmpeg": True,
+            "nocheckcertificate": True,
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "320",
+                }
+            ],
+            "outtmpl": "%(id)s.mp3",
+            "quiet": True,
+            "logtostderr": False,
+        }
+    try:
+        await event.edit("`Getting info...`")
+        with YoutubeDL(opts) as rip:
+            rip_data = rip.extract_info(url)
+    except DownloadError as DE:
+        await event.edit(f"`{str(DE)}`")
+        return
+    except ContentTooShortError:
+        await event.edit("`The download content was too short.`")
+        return
+    except GeoRestrictedError:
+        await event.edit("`Video is not available from your geographic location due to"
+                         + " geographic restrictions imposed by a website.`"
+                         )
+        return
+    except MaxDownloadsReached:
+        await event.edit("`Max-downloads limit has been reached.`")
+        return
+    except PostProcessingError:
+        await event.edit("`There was an error during post processing.`")
+        return
+    except UnavailableVideoError:
+        await event.edit("`Media is not available in the requested format.`")
+        return
+    except XAttrMetadataError as XAME:
+        return await event.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
+    except ExtractorError:
+        return await event.edit("`There was an error during info extraction.`")
+    except Exception as e:
+        return await event.edit(f"{str(type(e)): {str(e)}}")
+    dir = os.listdir()
+    if f"{rip_data['id']}.mp3.jpg" in dir:
+        thumb = f"{rip_data['id']}.mp3.jpg"
+    elif f"{rip_data['id']}.mp3.webp" in dir:
+        thumb = f"{rip_data['id']}.mp3.webp"
+    else:
+        thumb = None
+    upteload = """
+Uploading...
+Song name - {}
+By - {}
+""".format(
+        rip_data["title"], rip_data["uploader"]
+    )
+    await event.edit(f"`{upteload}`")
+    CAPT = f"⫸ Song - {rip_data['title']}\n⫸ By - {rip_data['uploader']}\n"
+    await event.client.send_file(
+        event.chat_id,
+        f"{rip_data['id']}.mp3",
+        thumb=thumb,
+        supports_streaming=True,
+        caption=CAPT,
+        attributes=[
+            DocumentAttributeAudio(
+                duration=int(rip_data["duration"]),
+                title=str(rip_data["title"]),
+                performer=str(rip_data["uploader"]),
+            )
+        ],
+    )
+    await event.delete()
+    os.remove(f"{rip_data['id']}.mp3")
+    try:
+        os.remove(thumb)
+    except BaseException:
+        pass
+
+
+@register(outgoing=True, pattern=r"^\.lirik (.*)")
+async def original(event):
+    if not event.pattern_match.group(1):
+        return await event.edit("give query to search.")
+    vcky = event.pattern_match.group(1)
+    event = await event.edit("Getting lyrics..")
+    dc = random.randrange(1, 3)
+    if dc == 1:
+        fromvt = "AIzaSyAyDBsY3WRtB5YPC6aB_w8JAy6ZdXNc6FU"
+    if dc == 2:
+        fromvt = "AIzaSyBF0zxLlYlPMp9xwMQqVKCQRq8DgdrLXsg"
+    if dc == 3:
+        fromvt = "AIzaSyDdOKnwnPwVIQ_lbH5sYE4FoXjAKIQV0DQ"
+    extract_lyrics = sl(f"{fromvt}", "15b9fb6193efd5d90")
+    sh1vm = extract_lyrics.get_lyrics(f"{vcky}")
+    a7ul = sh1vm["lyrics"]
+    await event.client.send_message(event.chat_id, a7ul, reply_to=event.reply_to_msg_id)
+    await event.delete()
+
+# For FromVT-Userbot
+# Piki Ganteng
+
+CMD_HELP.update(
+    {
+        "song": ">`.song <Judul Lagu>`"
+        "\nUsage: Mendownload Music"
+        ">`.lirik` <Judul Lagu>`"
+        "\nUsage: `mendapatkan lirik lagu`"
+    }
+)

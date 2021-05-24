de9f1a6dee204207e30df027474ba07c3fc76957
Red Devil <82816642+Naysilaa@users.noreply.github.com>
2021-05-24 15:56:25 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -262,7 +262,8 @@ async def amireallyalive(alive):
     await bot.get_me()
     await get_readable_time((time.time() - StartTime))
     await alive.edit("⚡𝙎𝙄𝙎𝙏𝙀𝙈 𝙈𝙀𝙉𝙔𝘼𝙇𝘼")
-    await alive.edit("✨")
+    await alive.edit("💡")
+    await asyncio.sleep(4)
     output = (
         f"╭┈──────────────────┈╮ \n"
         f"**      ⚡️ 𝗙 𝗟 𝗔 𝗦 𝗛 - 𝗠 𝗔 𝗫 𝗜 💢  **\n"
